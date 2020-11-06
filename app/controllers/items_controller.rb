6887694b10e7e161cb452123f35d763f03394ba6
n622ryo <fcv53@gmail.com>
2020-11-06 20:14:47 +0900

    Active Storageと画像加工ツールの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+  end
+
+  private
+
+  def message_params
+    params.require(:item).permit(:product, :description, :price, :category_id, :status_id, :burden_id, :area_id, :deliveryday_id, :image).merge(user_id: current_user.id)
+  end
 end
