1bf220cc87e9b48e416f98540931af8e1819c179
n622ryo <fcv53@gmail.com>
2020-11-06 10:43:50 +0900

    データベース修正

--- db/migrate/20201104022453_devise_create_users.rb
+++ db/migrate/20201104022453_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name,          null: false
       t.string :first_name_reading, null: false
       t.string :last_name_reading,  null: false
-      t.string :birthday,           null: false
+      t.date :birthday,             null: false
 
 
       ## Recoverable
--- db/schema.rb
+++ db/schema.rb
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_022453) do
     t.string "last_name", null: false
     t.string "first_name_reading", null: false
     t.string "last_name_reading", null: false
-    t.string "birthday", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
