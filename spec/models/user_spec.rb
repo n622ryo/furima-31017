8e108e13e94120631414b241c5ed0cbda4a88d85
n622ryo <fcv53@gmail.com>
2020-11-06 13:00:29 +0900

    emailのテストコードの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -48,6 +48,17 @@ RSpec.describe User, type: :model do
        @user.valid?
        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it "emailに＠が無いと登録できない" do
+       @user.email = "aaa.com"  
+       @user.valid?
+       expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it "重複したemailが存在する場合登録できない" do 
+       @user.save
+       another_user = FactoryBot.build(:user, email: @user.email)  
+       another_user.valid?
+       expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
       it "passwordが空では登録できない" do
        @user.password = ""  # passwordの値を空にする
        @user.valid?
