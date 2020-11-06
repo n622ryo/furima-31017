52e22a0f93c8425e7b3b3d1aff8737e90a9baa84
n622ryo <fcv53@gmail.com>
2020-11-06 10:45:05 +0900

    正規表現記入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,           presence: true
-  validates :first_name,         presence: true
-  validates :last_name,          presence: true
-  validates :first_name_reading, presence: true
-  validates :last_name_reading,  presence: true
-  validates :birthday,           presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_reading
+      validates :last_name_reading
+    end
+  end
 end
