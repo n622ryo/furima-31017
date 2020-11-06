057d93083d73ea651af02dc2e8b126f53b84df1a
n622ryo <fcv53@gmail.com>
2020-11-06 10:45:52 +0900

    Factory.bot修正・追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {"abe"}
     email                 {"kkk@gmail.com"}
-    password              {"00000000"}
+    password              {"111aaa"}
     password_confirmation {password}
+    first_name            {"阿部"}
+    last_name             {"直樹"}
+    first_name_reading    {"アベ"}
+    last_name_reading     {"ナオキ"}
+    birthday              {"1996-1-1"}
   end
 end
