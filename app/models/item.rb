ec2bb57527a8bafd479911dd4e561eaaaffad0f4
n622ryo <fcv53@gmail.com>
2020-11-07 14:22:07 +0900

    モデルの作成、アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
+  #通常のアソシエーション
   belongs_to :user
   has_one_attached :image
 
+  #ActiveHashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :deliveryday
+
   validates :image, presence: true
+
 end
--- spec/factories/burdens.rb
+++ spec/factories/burdens.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :genre do
+  factory :burden do
     
   end
 end
