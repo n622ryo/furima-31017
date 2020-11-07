a01f5366b1c3a32b422c68248e642d11f74f6bca
n622ryo <fcv53@gmail.com>
2020-11-07 22:07:31 +0900

    createアクション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
 
-  def message_params
+  def item_params
     params.require(:item).permit(:product, :description, :price, :category_id, :status_id, :burden_id, :area_id, :deliveryday_id, :image).merge(user_id: current_user.id)
   end
+  
 end
