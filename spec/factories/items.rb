a13cc826058de0327e32ba065ae2d009141eb7fe
n622ryo <fcv53@gmail.com>
2020-11-09 21:20:02 +0900

    単体テストコードの実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    product             {"商品名"}
+    description         {"商品の説明"}
+    category_id         { 2 }
+    status_id           { 2 }
+    burden_id           { 2 }
+    area_id             { 2 }
+    deliveryday_id      { 2 }
+    price               { 300 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,71 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)  # Itemのインスタンス生成
+  end
+
+  describe '商品出品機能' do
+    context '商品出品ががうまくいく時' do
+      it "imageとproduct、description、category_id、status_id、burden_id、area_id、deliveryday_id、priceが存在すれば登録できる" do
+       expect(@item).to be_valid
+      end
+      it "販売価格が半角数字であれば出品できる" do
+        @item.price = "300"
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品出品ががうまくいかない時' do
+      it "商品画像をつけていないと出品できない" do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it "商品名がないと出品できない" do
+        @item.product = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product can't be blank")
+      end
+      it "商品説明文がないと出品できない" do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it "カテゴリーの情報がないと出品できない" do
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
+      end
+      it "商品の状態についての情報がないと出品できない" do
+        @item.status_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank", "Status is not a number")
+      end
+      it "配送料の負担についての情報がないと出品できない" do
+        @item.burden_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank", "Burden is not a number")
+      end
+      it "発送元の負担についての情報がないと出品できない" do
+        @item.area_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+      end
+      it "発送までの日数についての情報がないと出品できない" do
+        @item.deliveryday_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Deliveryday can't be blank", "Deliveryday is not a number")
+      end
+      it "販売価格についての情報がないと出品できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it "販売価格が半角数字以外の情報の場合は出品できない" do
+        @item.price = "あaA１２３"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+    end
+  end
 end
