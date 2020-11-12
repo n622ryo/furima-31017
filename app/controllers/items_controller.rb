03bbb1b55b76ce50d335f272e42269ca727c354c
n622ryo <fcv53@gmail.com>
2020-11-12 20:35:16 +0900

    出品したユーザーでないと削除できないという条件分岐（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,7 +37,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    unless current_user == @item.user
+    if current_user == @item.user
       @item.destroy
       redirect_to root_path
     end
